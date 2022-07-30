b1153a88166c426674626946e537e69473ed0261
mahelhelou <mahelhelou@gmail.com>
2022-07-30 19:44:43 +0300

    Update 2022-07-30T16:44:33.020Z

---
+++ assets/pug/page-faqs.php
@@ -0,0 +1,34 @@
+<?php get_header(); ?>
+
+<!-- Page Banner-->
+<section class="page-banner">
+  <div class="page-banner__content">
+    <h1 class="page-banner__title">Most Frequently Asked Questions</h1>
+  </div>
+</section>
+<!-- page-banner-end-->
+
+<section class="faqs py-5">
+  <div class="container">
+    <div class="row">
+      <?php
+			$args = [
+				'post_type'				=> 'faq',
+				'posts_per_page'	=> -1
+			];
+
+			$faqs = new WP_Query($args);
+
+			if ( $faqs->have_posts() ) {
+				while ( $faqs->have_posts() ) {
+					$faqs->the_post();
+					get_template_part( 'template-parts/content/faq' );
+				}
+			}
+				wp_reset_postdata();
+			?>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
